98e2dcc8fb98bfd25ef180998dd6d6ca3da9ce73
John Blesson-Rowe <106766947+Johnblesson@users.noreply.github.com>
2023-12-07 14:55:05 +0000

    functions

---
+++ functions.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+// Functions
+// Functions are the building blocks of smart contracts
+// Functions are used to create the logic of the smart contract
+// A function is a piece of code that performs a specific task
+// Functions are used to create the logic of the smart contract
+// A function is basically a group of code that can be reused anywhere in the smart contract, or in other smart contracts.
+
+// In Solidity, functions are blocks of code that can be defined within a 
+// smart contract and are used to perform specific tasks or operations. 
+// They are fundamental building blocks of smart contracts and enable 
+// the execution of specific actions when called. Functions can be used.
+
+// Functions are the building blocks of smart contracts
+// Functions are used to create the logic of the smart contract.
+
+// Function Types
+// There are two types of functions in Solidity:
+// --- Declared functions
+// --- Undeclared functions
+
+// Declared functions
+// Declared functions are functions that are declared within the smart contract.
+
+// Declaring a function
+// Modifying state variables
+// Emitting events
+// Creating new contracts
+// Calling other functions
+// Sending Ether (or tokens) via calls
+// Returning data from the function
+// Declaring a function
+// Marking a function as pure or view
+// Assembly certain opcodes
+// Using low-level calls
+
+// Undeclared functions
+// Undeclared functions are functions that are not declared within the smart contract.
